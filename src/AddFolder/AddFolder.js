49a02186e30b18ea7858ba9bada35177d7e47b59
douniad <59784097+douniad@users.noreply.github.com>
2021-06-15 13:14:46 +0200

    Update AddFolder.js

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -14,7 +14,7 @@ export default function AddFolder(props) {
             return (
                 <form onSubmit={onFolderSubmit}>
                     <label>Folder Name</label>
-                    <input type="text" name="name" required="required"/>
+                    <input type="text" name="name"/>
                     <button>Save Folder</button>
                 </form>
             )
